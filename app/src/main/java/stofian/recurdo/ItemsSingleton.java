342c3aa15b19600075c867ce05ca21de85351608
MrSAndersson <stefan.andersson@nepa.com>
2018-09-21 17:24:07 +0200

    Create a NewItem activity

--- app/src/main/java/stofian/recurdo/ItemsSingleton.java
+++ app/src/main/java/stofian/recurdo/ItemsSingleton.java
@@ -1,14 +1,21 @@
 package stofian.recurdo;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+interface IItem {
+    void addItem(String name, List<Integer> weekdays);
+    String getName();
+    Integer getInterval();
+    boolean matchDay(Integer day);
+    void setWeekdays(List<Integer> weekdays);
+}
+
+
 class ItemsSingleton {
     private static ItemsSingleton itemsInstance;
-    private List<Item> items = new ArrayList<>();
+    private List<IItem> items = new ArrayList<>();
 
 
     static synchronized ItemsSingleton getInstance() {
@@ -19,11 +26,11 @@ class ItemsSingleton {
         return itemsInstance;
     }
 
-    public void addItem(String name, List<Integer> weekdays) {
-        this.items.add(new Item(name, weekdays));
+    public void addItem(IItem newItem) {
+        this.items.add(newItem);
     }
 
-    private Optional<Item> findItem(String name) {
+    private Optional<IItem> findItem(String name) {
 
         for (int x=0 ; x<items.size() ; x++) {
             if ( items.get(x).getName().equals(name) ) {
@@ -35,29 +42,29 @@ class ItemsSingleton {
 }
 
 
-class Item {
+class Item implements IItem{
     private String name;
     private List<Integer> weekdays;
     private Integer interval;
 
-    Item(String name, List<Integer> weekdays) {
+    public void addItem(String name, List<Integer> weekdays) {
         this.name = name;
         this.weekdays = weekdays;
     }
 
-    String getName() {
+    public String getName() {
         return name;
     }
 
-    Integer getInterval() {
+    public Integer getInterval() {
         return interval;
     }
 
-    boolean matchDay(Integer day) {
+    public boolean matchDay(Integer day) {
         return weekdays.contains(day);
     }
 
-    void setWeekdays(List<Integer> weekdays) {
+    public void setWeekdays(List<Integer> weekdays) {
         this.weekdays = weekdays;
     }
 
