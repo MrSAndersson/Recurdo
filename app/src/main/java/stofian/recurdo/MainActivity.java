bff90247adcd9c4fc7f9e5a0bb219d9bb448db3a
MrSAndersson <stefan.nigma@gmail.com>
2018-09-26 21:43:13 +0200

    Add IUpdatedGoalData interface to MainActivity

--- app/src/main/java/stofian/recurdo/MainActivity.java
+++ app/src/main/java/stofian/recurdo/MainActivity.java
@@ -11,17 +11,20 @@ import android.view.MenuItem;
 
 import stofian.recurdo.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
-    GoalSingleton goals = GoalSingleton.getInstance();
+interface IUpdatedGoalData {
+    void UpdatedGoalData();
+}
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener, IUpdatedGoalData{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements ItemFragment.OnLi
 
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+
+    @Override
+    public void UpdatedGoalData() {
 
     }
 }
